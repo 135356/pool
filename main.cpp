d0399102c1941abe6a2fb8948d65e292f331ae1f
longbang <x135356@sina.com>
2022-05-12 14:50:02 +0800

    linux编译问题修复(项目名称错误)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
+#include <iostream>
 #include "bb/pool.h"
 
 int main() {
-    bb::pool::obj(100).push([](){
-        printf("test\n");
-    });
-    bb::pool::objWait().push([](){
+    bb::pool::obj().push([](){
         printf("test\n");
     });
     return 0;
